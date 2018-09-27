42dd2a6c5982dfdda176642a97a70b80819a4f5f
alejandroclose@gmail.com <alejandroclose@gmail.com>
2018-09-27 19:42:04 +0200

    state managed from the outside

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    value: 'estado inicial'
+  }
+
+handleInput = (e) =>{
+  console.log(e.target.value)
+  this.setState({
+    value: e.target.value,
+  })
+}
   render() {
+    const {value} = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +24,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <input type="text" value={value} onChange={this.handleInput}/>
       </div>
     );
   }
